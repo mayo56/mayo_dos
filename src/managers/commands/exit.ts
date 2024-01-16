374998d0eb08bbcbe426cf4fd23ca88d6fba64ad
Mayo56 <maxinou56.lr@gmail.com>
2024-01-16 21:01:10 +0100

    (Modif banale)

--- src/managers/commands/exit.ts
+++ src/managers/commands/exit.ts
@@ -1,14 +1,25 @@
 import { commandReturn, commands } from "../../assets/types";
-
+import PrinterManager from "../printerManager";
 
 export default {
     name: "shutdown",
     aliases: ["sd"],
     exec(commandsArgs: string[], path: string): commandReturn {
+        const printer = new PrinterManager();
+
+        if (["-h", "--help"].includes(commandsArgs[0])) {
+            printer.message("Aide pour la commande d");
+            return {
+                "error": false,
+                "path": path,
+                "cancel": false,
+            };
+        };
+
         return {
             "error": false,
             "path": path,
-            "cancel": false,
-        }
+            "cancel": true,
+        };
     },
 } as commands;
